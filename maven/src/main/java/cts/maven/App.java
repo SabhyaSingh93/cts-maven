028c16fa7a50e7aaf68cafd2845c204f6f7ee280
SabhyaSingh93 <sabhyasiet087@gmail.com>
2021-02-24 18:23:15 +0530

    initial commit

---
+++ maven/src/main/java/cts/maven/App.java
@@ -0,0 +1,13 @@
+package cts.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
